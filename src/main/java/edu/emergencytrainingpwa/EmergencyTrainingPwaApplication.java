c8091fac99630dffff00bdf040aab31c71031c50
Kostashchuk Iryna <kostashchuk.irina@gmail.com>
2025-04-06 18:07:27 +0300

    initial commit

---
+++ src/main/java/edu/emergencytrainingpwa/EmergencyTrainingPwaApplication.java
@@ -0,0 +1,13 @@
+package edu.emergencytrainingpwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyTrainingPwaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyTrainingPwaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/emergencytrainingpwa/EmergencyTrainingPwaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.emergencytrainingpwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyTrainingPwaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
